f91bda52b58fbb5e8b3a4d6588bb48634aa7968b
Sahan2002-cmd <it23837676@.my.sliit.lk>
2026-04-23 11:43:11 +0530

    fix errors after add github

--- GymManagement/App_Start/UnityConfig.cs
+++ GymManagement/App_Start/UnityConfig.cs
@@ -1,6 +1,9 @@
+using System.Web.Mvc;
 using System.Web.Http;
 using Unity;
 using Unity.WebApi;
+using GymManagement.Interfaces;
+using GymManagement.DataAccess;
 
 namespace GymManagement
 {
@@ -8,14 +11,37 @@ namespace GymManagement
     {
         public static void RegisterComponents()
         {
-			var container = new UnityContainer();
+            var container = new UnityContainer();
             
-            // register all your components with the container here
-            // it is NOT necessary to register your controllers
+            // Register DataAccess implementations
+            container.RegisterType<IAttendance, DAAttendance>();
+            container.RegisterType<IComplaint, DAComplain>();
+            container.RegisterType<IDevice, DADevice>();
+            container.RegisterType<IEquipment, DAEquipment>();
+            container.RegisterType<IEquipmentAssignment, DAEquipmentAssignment>();
+            container.RegisterType<IEquipmentUsageLog, DAEquipmentUsageLog>();
+            container.RegisterType<IExercise, DAExercise>();
+            container.RegisterType<IMember, DAMember>();
+            container.RegisterType<INonEquipmentExercise, DANonEquipmentExercise>();
+            container.RegisterType<IParQ, DAParQ>();
+            container.RegisterType<IPayment, DAPayment>();
+            container.RegisterType<IPlan, DAPlan>();
+            container.RegisterType<IReport, DAReport>();
+            container.RegisterType<IRfidTag, DARfidTag>();
+            container.RegisterType<ISchedule, DASchedule>();
+            container.RegisterType<ISubscription, DASubscription>();
+            container.RegisterType<ITimeSlot, DATimeSlot>();
+            container.RegisterType<ITrainer, DATrainer>();
+            container.RegisterType<ITrainerAssignment, DATrainerAssignment>();
+            container.RegisterType<ITrainerAttendance, DATrainerAttendance>();
+            container.RegisterType<ITrainerTimeSlot, DATrainerTimeSlot>();
+            container.RegisterType<IUser, DAUser>();
             
-            // e.g. container.RegisterType<ITestService, TestService>();
+            // Set Web API resolver
+            System.Web.Http.GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
             
-            GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
+            // Set MVC resolver
+            DependencyResolver.SetResolver(new Unity.Mvc5.UnityDependencyResolver(container));
         }
     }
 }
